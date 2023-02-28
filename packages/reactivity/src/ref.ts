466d358c95bfd74378318b77c17755f5528514e9
xing-6464 <xing_6464@foxmail.com>
2023-02-28 22:49:09 +0800

    实现ref 简单数据类型的响应性

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,5 +1,6 @@
+import { hasChanged } from '@vue/shared'
 import { createDep, Dep } from './dep'
-import { activeEffect, trackEffects } from './effect'
+import { activeEffect, trackEffects, triggerEffects } from './effect'
 import { toReactive } from './reactive'
 
 export interface Ref<T = any> {
@@ -20,11 +21,13 @@ function createRef(rawValue: unknown, shallow: boolean) {
 
 class RefImpl<T> {
   private _value: T
+  private _rawValue: T
 
   public dep?: Dep = undefined
   public readonly __v_isRef = true
 
   constructor(value: T, public readonly __v_isShallow: boolean) {
+    this._rawValue = value
     this._value = __v_isShallow ? value : toReactive(value)
   }
 
@@ -33,12 +36,32 @@ class RefImpl<T> {
     return this._value
   }
 
-  set value(newValue) {}
+  set value(newVal) {
+    if (hasChanged(newVal, this._rawValue)) {
+      this._rawValue = newVal
+      this._value = toReactive(newVal)
+      triggerRefValue(this)
+    }
+  }
 }
 
+/**
+ * 收集依赖
+ * @param ref RefImpl
+ */
 export function trackRefValue(ref) {
   if (activeEffect) {
     trackEffects(ref.dep || (ref.dep = createDep()))
+  }
+}
+
+/**
+ * 触发依赖
+ * @param ref RefImpl
+ */
+export function triggerRefValue(ref) {
+  if (ref.dep) {
+    triggerEffects(ref.dep)
   }
 }
 
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,12 @@
 export const isArray = Array.isArray
 export const isObject = (val: unknown) =>
   val !== null && typeof val === 'object'
+
+/**
+ * 对比两个数据是否发生改变
+ * @param value any
+ * @param oldValue any
+ * @returns boolean
+ */
+export const hasChanged = (value: any, oldValue: any): boolean =>
+  !Object.is(value, oldValue)
