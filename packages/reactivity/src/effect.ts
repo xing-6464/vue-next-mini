ce0124b719b6eec49135da599e26c87ba9a85734
xing-6464 <xing_6464@foxmail.com>
2023-03-01 01:51:08 +0800

    computed的缓存实现

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -87,7 +87,15 @@ export function triggerEffects(dep: Dep) {
 
   // 触发依赖
   for (const effect of effects) {
-    triggerEffect(effect)
+    if (effect.computed) {
+      triggerEffect(effect)
+    }
+  }
+
+  for (const effect of effects) {
+    if (!effect.computed) {
+      triggerEffect(effect)
+    }
   }
 }
 
