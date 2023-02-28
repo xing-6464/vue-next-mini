c0dfe5df7c27a6119008d1c5c13c9113ac9a76a9
xing-6464 <xing_6464@foxmail.com>
2023-02-28 18:36:30 +0800

    reactivity

--- rollup.config.js
+++ rollup.config.js
@@ -2,33 +2,34 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 
+/**
+ * 默认导出一个数组，数组的每一个对象都是一个单独的导出文件配置，详细可查：https://www.rollupjs.com/guide/big-list-of-options
+ */
 export default [
   {
-    // 人口文件
+    // 入口文件
     input: 'packages/vue/src/index.ts',
     // 打包出口
     output: [
-      // 导出iife 模式的包
+      // 导出 iife 模式的包
       {
-        // 开启sourceMap
+        // 开启 SourceMap
         sourcemap: true,
-        // 导出文件地址
+        // 导出的文件地址
         file: './packages/vue/dist/vue.js',
-        // 生成包的格式
+        // 生成的包格式：一个自动执行的功能，适合作为<script>标签
         format: 'iife',
         // 变量名
-        name: 'vue'
+        name: 'Vue'
       }
     ],
     // 插件
-    plugin: [
-      //ts
-      typescript({
-        sourceMap: true
-      }),
+    plugins: [
+      // ts 支持
+      typescript({ sourceMap: true }),
       // 模块导入的路径补全
       resolve(),
-      // 转 commonjs 为ESM
+      // 将 CommonJS 模块转换为 ES2015
       commonjs()
     ]
   }
