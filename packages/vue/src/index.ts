efe3c88f05519b3269602ad87c022d39018bebf3
xing-6464 <xing_6464@foxmail.com>
2023-02-28 16:09:35 +0800

    响应性

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,3 +1,26 @@
-import { isArray } from '@vue/shared'
+let quantity = 2
+let product = {
+  price: 10,
+  quantity: quantity
+}
 
-console.info('hello')
+let total = 0
+
+let effect = () => {
+  total = product.price * product.quantity
+}
+
+effect()
+console.info(`总价格：${total}`)
+
+Object.defineProperty(product, 'quantity', {
+  set(newValue) {
+    console.info('set')
+    quantity = newValue
+    effect()
+  },
+  get() {
+    console.info('get')
+    return quantity
+  }
+})
